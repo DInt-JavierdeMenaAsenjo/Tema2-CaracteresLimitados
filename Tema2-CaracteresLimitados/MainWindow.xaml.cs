0e5a8a94b1a4b735a8829eb0c426ef05fe6a3a28
JAVIER DE MENA ASENJO <javdemase@alu.edu.gva.es>
2022-09-29 12:57:10 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-CaracteresLimitados/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int contadorCaracteres = 0;
+        int ultimoTamaño;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ultimoTamaño = texto_TextBlock.Text.Length;
+            contador_TextBlock.Text = $"{contadorCaracteres} / 140";
+        }
+
+        private void texto_TextBlock_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int tamActual = texto_TextBlock.Text.Length;
+            if (ultimoTamaño > tamActual) contadorCaracteres--;
+            else contadorCaracteres++;
+
+            if (tamActual == 140) texto_TextBlock.IsReadOnly = true;
+
+            ultimoTamaño = tamActual;
+            contador_TextBlock.Text = $"{contadorCaracteres} / 140";
+        }
+
+
+        private void texto_TextBlock_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                texto_TextBlock.Text = texto_TextBlock.Text + "\n";
+                ultimoTamaño++;
+                texto_TextBlock.CaretIndex = ultimoTamaño;
+            }
+
+        }
+    }
+}
